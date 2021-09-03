45386530239eb0c7edbc211d0ed4fadaed8e372d
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-09-03 11:28:56 +0200

    ping works

--- src/client.cpp
+++ src/client.cpp
@@ -4,84 +4,60 @@
 
 #include <grpcpp/grpcpp.h>
 
-#include "../_generated/helloworld.pb.h"
-#include "../_generated/helloworld.grpc.pb.h"
+#include "../_generated/ping.pb.h"
+#include "../_generated/ping.grpc.pb.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
-using helloworld::Greeter;
-using helloworld::HelloReply;
-using helloworld::HelloRequest;
+using ping::PingReply;
+using ping::PingRequest;
+using ping::PingService;
 
-class GreeterClient {
+class PingClient {
  public:
-  GreeterClient(std::shared_ptr<Channel> channel)
-      : stub_(Greeter::NewStub(channel)) {}
+   PingClient(std::shared_ptr<Channel> channel)
+       : stub_(PingService::NewStub(channel)) {}
 
-  // Assembles the client's payload, sends it and presents the response back
-  // from the server.
-  std::string SayHello(const std::string& user) {
-    // Data we are sending to the server.
-    HelloRequest request;
-    request.set_name(user);
+   // Assembles the client's payload, sends it and presents the response back
+   // from the server.
+   bool Ping()
+   {
+     // Data we are sending to the server.
+     PingRequest request;
 
-    // Container for the data we expect from the server.
-    HelloReply reply;
+     // Container for the data we expect from the server.
+     PingReply reply;
 
-    // Context for the client. It could be used to convey extra information to
-    // the server and/or tweak certain RPC behaviors.
-    ClientContext context;
-
-    // The actual RPC.
-    Status status = stub_->SayHello(&context, request, &reply);
-
-    // Act upon its status.
-    if (status.ok()) {
-      return reply.message();
-    } else {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      return "RPC failed";
-    }
+     // Context for the client. It could be used to convey extra information to
+     // the server and/or tweak certain RPC behaviors.
+     ClientContext context;
+     // The actual RPC.
+     Status status = stub_->Ping(&context, request, &reply);
+     // Act upon its status.
+     if (status.ok())
+     {
+       return true;
+     }
+     else
+     {
+       std::cout << status.error_code() << ": " << status.error_message()
+                 << std::endl;
+       return false;
+     }
   }
 
  private:
-  std::unique_ptr<Greeter::Stub> stub_;
+   std::unique_ptr<PingService::Stub> stub_;
 };
 
 int main(int argc, char** argv) {
-  // Instantiate the client. It requires a channel, out of which the actual RPCs
-  // are created. This channel models a connection to an endpoint specified by
-  // the argument "--target=" which is the only expected argument.
-  // We indicate that the channel isn't authenticated (use of
-  // InsecureChannelCredentials()).
-  std::string target_str;
-  std::string arg_str("--target");
-  if (argc > 1) {
-    std::string arg_val = argv[1];
-    size_t start_pos = arg_val.find(arg_str);
-    if (start_pos != std::string::npos) {
-      start_pos += arg_str.size();
-      if (arg_val[start_pos] == '=') {
-        target_str = arg_val.substr(start_pos + 1);
-      } else {
-        std::cout << "The only correct argument syntax is --target="
-                  << std::endl;
-        return 0;
-      }
-    } else {
-      std::cout << "The only acceptable argument is --target=" << std::endl;
-      return 0;
-    }
-  } else {
-    target_str = "localhost:50051";
-  }
-  GreeterClient greeter(
+  std::string target_str = "localhost:50051";
+  
+  PingClient client(
       grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
-  std::string user("world");
-  std::string reply = greeter.SayHello(user);
-  std::cout << "Greeter received: " << reply << std::endl;
+  bool response = client.Ping();
+  std::cout << "ping response: " << response << std::endl;
 
   return 0;
 }
