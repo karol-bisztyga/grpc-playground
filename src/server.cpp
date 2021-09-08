b245f767f9fa2df33f9cd6e97ca379cf9099b5cb
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-09-08 11:26:37 +0200

    rewritten working

--- src/server.cpp
+++ src/server.cpp
@@ -10,7 +10,6 @@
 #include <grpcpp/health_check_service_interface.h>
 
 #include <chrono>
-using namespace std::chrono_literals;
 
 #include "../_generated/ping.pb.h"
 #include "../_generated/ping.grpc.pb.h"
@@ -72,7 +71,6 @@ public:
   }
 };
 
-// Logic and data behind the server's behavior.
 class PingServiceImpl final : public PingService::Service
 {
 public:
@@ -83,15 +81,10 @@ public:
     std::cout << "initialize" << std::endl;
     const size_t id = (size_t)request->id();
 
-    // response.set_initialresponsetype(InitialResponseType::WAIT);
-    // writer->Write(response);
-
-    //////////////////////////////////////////
     if (primaries.get(id) == nullptr)
     {
       // this device becomes a new primary device
       response->set_initialresponsetype(InitialResponseType::NEW_PRIMARY);
-      // writer->Write(response);
 
       std::shared_ptr<ClientData> clientData = std::make_shared<ClientData>((size_t)request->id());
       primaries.set(id, clientData);
@@ -99,28 +92,20 @@ public:
     else
     {
       response->set_initialresponsetype(InitialResponseType::PRIMARY_PRESENT);
-      // writer->Write(response);
     }
-    //////////////////////////////////////////
 
     return Status::OK;
   }
 
   Status SendPing(ServerContext *context, const SendPingRequest *request, SendPingResponse *response) override
   {
+    using namespace std::chrono_literals;
     std::cout << "send ping" << std::endl;
     const size_t id = (size_t)request->id();
-    // ClientData primarydeviceClient = primaries.get(clientData->id);
-
-    // there's another primary device registered already
-    // current client has to wait for the pong from the client
-    // that owns the current primary device
-
     // add a ping request for the client that owns a primary device
     primaries.get(id)->pingRequests.enqueue(true);
 
     // check for a pong with a timeout...
-    // std::cout << "waiting for the pong from the primary device" << std::endl;
     std::unique_lock<std::mutex> lock(primaries.get(id)->mutex);
     if (primaries.get(id)->cv.wait_for(lock, 500ms, [=]
                                                    { return primaries.get(id)->isActive; }))
@@ -133,12 +118,6 @@ public:
       std::cout << "timed out" << std::endl;
       response->set_sendpingresponsetype(SendPingResponseType::PRIMARY_OFFLINE);
     }
-    // TODO: the question here is: should we terminate the client or leave it
-    // so it becomes a primary device once the original one perishes?
-    // to do that we'd probably need an additional queue of primary candidates
-    // and a non busy loop here(also some additional fields in ResponseType)
-    // for now it terminates
-
     return Status::OK;
   }
 
@@ -163,40 +142,6 @@ public:
 
     return Status::OK;
   }
-  /*
-  Status Ping(ServerContext *context, PingRequest *reader, ServerWriter<PingResponse> *response) override
-  {
-    std::cout << "server ping" << std::endl;
-
-    // here we want to ping a client that owns a device that's marked as primary
-    PingRequest request;
-    reader->Read(&request);
-    const size_t id = request.id();
-
-    std::cout << "ping a primary device with id " << id << std::endl;
-
-    return Status::OK;
-  }
-
-  / *
-  // every connection goes on a separate thread
-  Status Ping(ServerContext *context, ServerReaderWriter<PingResponse, PingRequest> *reactor) override
-  {
-    std::cout << "here server ping #start " << std::this_thread::get_id() << std::endl;
-
-    std::mutex m;
-    std::condition_variable cv;
-
-    PingRequest request;
-    PingResponse response;
-    reactor->Read(&request);
-
-    std::cout << "here server ping #read, client id: " << clientData->id << std::endl;
-
-    std::cout << "terminating client" << std::endl;
-    return Status::OK;
-  }
-  */
 };
 
 void RunServer()
