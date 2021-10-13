82fa8c663d0c1cff85b24b88ed2549bccc5f4675
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-10-13 15:43:51 +0200

    connection works

--- src/TunnelBrokerClient.h
+++ src/TunnelBrokerClient.h
@@ -5,12 +5,16 @@
 #include "../_generated/tunnelbroker.pb.h"
 #include "../_generated/tunnelbroker.grpc.pb.h"
 
+#include "../_generated/backup.pb.h"
+#include "../_generated/backup.grpc.pb.h"
+
 #include <string>
 #include <memory>
 
 class TunnelBrokerClient
 {
-  std::unique_ptr<tunnelbroker::TunnelBrokerService::Stub> stub_;
+  std::unique_ptr<tunnelbroker::TunnelBrokerService::Stub> tbStub_;
+  std::unique_ptr<tunnelbroker::BackupService::Stub> backupStub_;
   const std::string id;
   const std::string deviceToken;
 public:
@@ -18,4 +22,8 @@ public:
   tunnelbroker::CheckResponseType checkIfPrimaryDeviceOnline();
   bool becomeNewPrimaryDevice();
   void sendPong();
+
+  void sendLog(const std::string data);
+  void resetLog();
+  void restoreBackup();
 };
