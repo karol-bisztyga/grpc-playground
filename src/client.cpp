797e17f3117a770a7e090d10341f33fef7411ff1
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-10-14 10:29:11 +0200

    backup works

--- src/client.cpp
+++ src/client.cpp
@@ -2,26 +2,20 @@
 
 #include <grpcpp/grpcpp.h>
 
-#include "../_generated/tunnelbroker.pb.h"
-#include "../_generated/tunnelbroker.grpc.pb.h"
-
 #include <iostream>
-#include <memory>
+#include <random>
 #include <string>
 
-#include <chrono>
-
-using namespace std::chrono;
-
-// this is a simulation of a device token
-// I figured we can use a timestamp in milliseconds as a unique id for a process
-// I'm going to assume it's not possible to launch multiple processes
-// at the same very millisecond
-long long generateUID()
+std::string randomString()
 {
-  milliseconds ms = duration_cast<milliseconds>(
-      system_clock::now().time_since_epoch());
-  return ms.count();
+  std::string str("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+
+  std::random_device rd;
+  std::mt19937 generator(rd());
+
+  std::shuffle(str.begin(), str.end(), generator);
+
+  return str.substr(0, 32);
 }
 
 int main(int argc, char **argv)
@@ -32,80 +26,50 @@ int main(int argc, char **argv)
     return 1;
   }
   std::string id = std::string(argv[1]);
-  std::string deviceToken = std::to_string(generateUID());
   std::cout << "client start" << std::endl;
   std::cout << "id           : " << id << std::endl;
-  std::cout << "device token : " << deviceToken << std::endl;
   std::string target_str = "localhost:50051";
 
-  TunnelBrokerClient client(
+  Client client(
       grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()),
-      id,
-      deviceToken);
+      id);
 
   char option = '?';
   while (option != 'e')
   {
-    std::string options = "cbplrbe";
+    std::string options = "srbe";
     std::cout << "what you want to do?" << std::endl;
-    std::cout << "[c] *ping* check if primary device is online" << std::endl;
-    std::cout << "[n] *ping* become a new primary device" << std::endl;
-    std::cout << "[p] *ping* send pong" << std::endl;
-    std::cout << "[l] *backup* send log" << std::endl;
-    std::cout << "[r] *backup* reset log" << std::endl;
-    std::cout << "[b] *backup* restore backup" << std::endl;
+    std::cout << "[s] send log" << std::endl;
+    std::cout << "[r] reset log" << std::endl;
+    std::cout << "[b] restore backup" << std::endl;
     std::cout << "[e] exit" << std::endl;
     std::cin >> option;
     if (options.find(option) == std::string::npos)
     {
       std::cout << "invalid command [" << option << "], skipping" << std::endl;
+      continue;
     }
     try
     {
       switch (option)
       {
-      case 'c':
+      case 's':
       {
-        tunnelbroker::CheckResponseType checkResponse = client.checkIfPrimaryDeviceOnline();
-        std::cout << "check primary device response: " << tunnelbroker::CheckResponseType_Name(checkResponse) << std::endl;
-        break;
-      }
-      case 'n':
-      {
-        bool success = client.becomeNewPrimaryDevice();
-        std::cout << "trying to become a new primary device... ";
-        if (success)
-        {
-          std::cout << "success";
-        }
-        else
-        {
-          std::cout << "failed";
-        }
-        std::cout << std::endl;
-        break;
-      }
-      case 'p':
-      {
-        std::cout << "sending pong... ";
-        client.sendPong();
-        std::cout << "sent!" << std::endl;
-        break;
-      }
-      case 'l':
-      {
-        std::cout << "sending log... ";
-        client.sendLog("hello!");
+        const std::string data = randomString() + " ";
+        std::cout << "sending log: [" << data << "]" << std::endl;
+        client.sendLog(data);
         break;
       }
       case 'r':
       {
-        std::cout << "reseting log... ";
+        std::cout << "reseting log... " << std::endl;
+        client.resetLog();
         break;
       }
       case 'b':
       {
-        std::cout << "restoring backup... ";
+        std::cout << "restoring backup... " << std::endl;
+        client.restoreBackup();
         break;
       }
       }
