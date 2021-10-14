2876c0ec4f5a902a1c4de29205893db32c0df934
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-10-14 10:29:11 +0200

    backup works

--- src/TunnelBrokerClient.h
+++ src/TunnelBrokerClient.h
@@ -2,26 +2,19 @@
 
 #include <grpcpp/grpcpp.h>
 
-#include "../_generated/tunnelbroker.pb.h"
-#include "../_generated/tunnelbroker.grpc.pb.h"
-
 #include "../_generated/backup.pb.h"
 #include "../_generated/backup.grpc.pb.h"
 
 #include <string>
 #include <memory>
 
-class TunnelBrokerClient
+class Client
 {
-  std::unique_ptr<tunnelbroker::TunnelBrokerService::Stub> tbStub_;
-  std::unique_ptr<tunnelbroker::BackupService::Stub> backupStub_;
+  std::unique_ptr<backup::BackupService::Stub> stub;
   const std::string id;
   const std::string deviceToken;
 public:
-  TunnelBrokerClient(std::shared_ptr<grpc::Channel> channel, std::string id, std::string deviceToken);
-  tunnelbroker::CheckResponseType checkIfPrimaryDeviceOnline();
-  bool becomeNewPrimaryDevice();
-  void sendPong();
+  Client(std::shared_ptr<grpc::Channel> channel, std::string id);
 
   void sendLog(const std::string data);
   void resetLog();
