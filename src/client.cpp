ff105b6120f2754d71cc5cd3e7b6e96417f5aa5b
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-10 14:50:44 +0100

    blob draft

--- src/client.cpp
+++ src/client.cpp
@@ -1,40 +1,34 @@
 #include "Client.h"
 
-Client::Client(std::shared_ptr<grpc::Channel> channel, std::string id)
-    : stub(backup::BackupService::NewStub(channel)),
-      id(id) {}
+Client::Client(std::shared_ptr<grpc::Channel> channel)
+    : stub(blob::BlobService::NewStub(channel)) {}
 
-void Client::resetKey(const std::string newKey, const std::vector<std::string> newCompact)
-{
+
+void Client::put(const std::string &reverseIndex, std::function<std::string()> &dataChunksObtainer) {
   grpc::ClientContext context;
+  blob::PutRequest request;
   ::google::protobuf::Empty response;
+  std::unique_ptr<grpc::ClientWriter<blob::PutRequest>> writer = this->stub->Put(&context, &response);
 
-  std::unique_ptr<grpc::ClientWriter<backup::ResetKeyRequest>> writer = this->stub->ResetKey(&context, &response);
-
-  backup::ResetKeyRequest request;
-  request.set_userid(this->id);
-
-  // first send a key and an empty chunk
-  request.set_newkey(newKey);
-    std::cout << "trying to write a new key [" << newKey << "]" << std::endl;
+  request.set_reverseindex(reverseIndex);
   if (!writer->Write(request))
   {
     std::cout << "stream interrupted" << std::endl;
     return;
   }
+  request.set_reverseindex("");
 
-  // then send an empty key and filled chunks
-  request.set_newkey("");
-  for (std::string chunk : newCompact)
-  {
-    std::cout << "trying to write chunk [" << chunk << "]" << std::endl;
-    request.set_compactionchunk(chunk);
+  size_t chunkCounter = 0;
+  do {
+    std::cout << "reading chunk " << ++chunkCounter << std::endl;
+    request.set_datachunk(dataChunksObtainer());
     if (!writer->Write(request))
     {
       std::cout << "stream interrupted" << std::endl;
       return;
     }
-  }
+  } while(request.datachunk().size());
+
   writer->WritesDone();
   grpc::Status status = writer->Finish();
   if (!status.ok())
@@ -42,78 +36,27 @@ void Client::resetKey(const std::string newKey, const std::vector<std::string> n
     throw std::runtime_error("writer error");
   }
   std::cout << "done writing chunks" << std::endl;
-  }
-
-void Client::sendLog(const std::string data)
-{
-  grpc::ClientContext context;
-  backup::SendLogRequest request;
-  ::google::protobuf::Empty response;
-
-  request.set_userid(this->id);
-  request.set_data(data);
-
-  std::cout << "sending log [" << data << "]" << std::endl;
-
-  grpc::Status status = this->stub->SendLog(&context, request, &response);
-  if (!status.ok())
-  {
-    throw std::runtime_error(status.error_message());
-  }
-  std::cout << "log sent" << std::endl;
 }
 
-std::string Client::pullBackupKey(const std::string pakeKey)
-{
+void Client::get(const std::string &reverseIndex, std::function<void(std::string)> &callback) {
   grpc::ClientContext context;
-  backup::PullBackupKeyRequest request;
-  backup::PullBackupKeyResponse response;
+  blob::GetRequest request;
+  blob::GetResponse response;
 
-  request.set_userid(this->id);
-  request.set_pakekey(pakeKey);
+  request.set_reverseindex(reverseIndex);
 
-  grpc::Status status = this->stub->PullBackupKey(&context, request, &response);
-  if (!status.ok())
-  {
-    throw std::runtime_error(status.error_message());
-  }
-
-  std::string backupKey = response.encryptedbackupkey();
-
-  std::cout << "pull backup key, received [" << backupKey << "]" << std::endl;
-
-  return backupKey;
-}
-
-CompactionResponse Client::pullCompact()
-{
-  grpc::ClientContext context;
-  backup::PullCompactionRequest request;
-  backup::PullCompactionResponse response;
-
-  request.set_userid(this->id);
-
-  std::unique_ptr<grpc::ClientReader<backup::PullCompactionResponse>> stream = this->stub->PullCompaction(&context, request);
-  std::cout << "reading compact stream:" << std::endl;
-
-  CompactionResponse compResponse;
+  std::unique_ptr<grpc::ClientReader<blob::GetResponse>> stream = this->stub->Get(&context, request);
+  std::cout << "reading stream:" << std::endl;
 
   while (stream->Read(&response))
   {
-    std::string compactionChunk = response.compactionchunk();
-    std::string logChunk = response.logchunk();
-    if (compactionChunk.size())
-    {
-      std::cout << "received[c]: [" << compactionChunk << "]" << std::endl;
-      compResponse.compaction += compactionChunk;
-    }
-    if (logChunk.size())
-    {
-      std::cout << "received[l]: [" << logChunk << "]" << std::endl;
-      compResponse.logs += logChunk;
-    }
+    std::string dataChunk = response.datachunk();
+    std::cout << "received: [" << dataChunk << "]" << std::endl;
+    callback(dataChunk);
   }
   std::cout << "done reading the restore stream" << std::endl;
+}
 
-  return compResponse;
+bool Client::remove(const std::string &reverseIndex) {
+  ;
 }
