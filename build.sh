88c0b3a63d4c2ecfc29a206424c591019ade2c07
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-02-04 12:04:10 +0100

    fix build script

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-mkdir cmake/build || echo "build folder exists, skipping"
+mkdir -p cmake/build || echo "build folder exists, skipping"
+
+./generate.sh
+
 cd cmake/build
 cmake ../..
 make
