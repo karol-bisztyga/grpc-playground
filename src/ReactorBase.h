0f5caf32a064b2ceec0bde344c024c67d80a6196
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-02-25 10:32:17 +0100

    move reactor to a separate file

---
+++ src/ReactorBase.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <grpcpp/grpcpp.h>
+#include <iostream>
+#include <string>
+
+template <class Request, class Response>
+class ReactorBase : public grpc::ServerBidiReactor<Request, Response> {
+  Request request;
+  Response response;
+
+  void finish(grpc::Status status = grpc::Status::OK);
+
+public:
+  ReactorBase();
+
+  void OnDone() override;
+  void OnReadDone(bool ok) override;
+  void OnWriteDone(bool ok) override;
+
+  virtual grpc::Status handleRequest(Request request, Response *response) = 0;
+};
+
+template <class Request, class Response>
+void ReactorBase<Request, Response>::finish(grpc::Status status) {
+  this->Finish(status);
+}
+
+template <class Request, class Response>
+ReactorBase<Request, Response>::ReactorBase() {
+  this->StartRead(&this->request);
+}
+
+template <class Request, class Response>
+void ReactorBase<Request, Response>::OnDone() {
+  delete this;
+}
+
+template <class Request, class Response>
+void ReactorBase<Request, Response>::OnReadDone(bool ok) {
+  if (!ok) {
+    this->finish(grpc::Status(grpc::StatusCode::INTERNAL, "reading error"));
+    return;
+  }
+  try {
+    grpc::Status status = this->handleRequest(this->request, &this->response);
+    if (status.ok()) {
+      this->finish(status);
+      return;
+    }
+    this->StartWrite(&this->response);
+  } catch (std::runtime_error &e) {
+    this->finish(grpc::Status(grpc::StatusCode::INTERNAL, e.what()));
+  }
+}
+
+template <class Request, class Response>
+void ReactorBase<Request, Response>::OnWriteDone(bool ok) {
+  if (!ok) {
+    std::cout << "Server write failed" << std::endl;
+    return;
+  }
+  this->StartRead(&this->request);
+}
--- src/server.cpp
+++ src/server.cpp
@@ -9,101 +9,27 @@
 #include "../_generated/example.pb.h"
 #include "../_generated/example.grpc.pb.h"
 
-struct EndConnectionError : public std::exception
-{
-  const char *what() const throw()
-  {
-    return "connection ended";
-  }
-};
-
-template <class Request, class Response>
-class ReactorBase : public grpc::ServerBidiReactor<Request, Response>
-{
-  Request request;
-  Response response;
-  bool finished = false;
-
-  void finish(grpc::Status status = grpc::Status::OK)
-  {
-    std::cout << "=> finish" << std::endl;
-    if (this->finished)
-    {
-      return;
-    }
-    this->finished = true;
-    this->Finish(status);
-  }
-
-public:
-  ReactorBase()
-  {
-    this->StartRead(&this->request);
-  }
-
-  void OnDone() override
-  {
-    std::cout << "=> done" << std::endl;
-    GPR_ASSERT(this->finished);
-    delete this;
-  }
-
-  void OnReadDone(bool ok) override
-  {
-    if (!ok)
-    {
-      this->finish();
-      return;
-    }
-    try
-    {
-      this->response = this->handleRequest(this->request);
-      this->StartWrite(&this->response);
-    }
-    catch (EndConnectionError &e)
-    {
-      this->finish();
-    }
-    catch (std::runtime_error &e)
-    {
-      std::cout << "error " << e.what() << std::endl;
-      this->finish(grpc::Status(grpc::StatusCode::INTERNAL, e.what()));
-    }
-  }
-
-  void OnWriteDone(bool ok) override
-  {
-    if (!ok)
-    {
-      gpr_log(GPR_ERROR, "Server write failed");
-      return;
-    }
-    this->StartRead(&this->request);
-  }
-
-  virtual Response handleRequest(Request request) = 0;
-};
+#include "ReactorBase.h"
 
 class ExchangeReactor : public ReactorBase<example::DataRequest, example::DataResponse>
 {
   std::vector<std::string> responses = {"", "res 4", "res 3", "res 2", "res 1"};
   size_t i=0;
 public:
-  example::DataResponse handleRequest(example::DataRequest request) override
+  grpc::Status handleRequest(example::DataRequest request, example::DataResponse *response) override
   {
     std::cout << "received: " << request.data() << "/ gonna respond: " << this->responses.back() << std::endl;
     if (this->responses.empty()) {
-      throw EndConnectionError();
+      return grpc::Status(grpc::StatusCode::CANCELLED, "empty response");
     }
     if (i>1)
     {
       throw std::runtime_error("test error");
     }
-    example::DataResponse response;
-    response.set_data(this->responses.back() + "/" + std::to_string(i));
+    response->set_data(this->responses.back() + "/" + std::to_string(i));
     this->responses.pop_back();
     ++i;
-    return response;
+    return grpc::Status::OK;
   }
 };
 
